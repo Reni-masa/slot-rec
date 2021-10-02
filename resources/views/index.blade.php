46323c3501d0217655167f9aa6f174004091ab0c
SugitaMasataka <mstk.ambit@gmail.com>
2021-10-02 17:48:49 +0900

    [install]debugbar導入

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>スロレク</title>
+</head>
+<body>
+    <h1>スロレク</h1>
+</body>
+</html>
