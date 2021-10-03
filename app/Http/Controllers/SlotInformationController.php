5c6f1589e2c7648a0c00b50dff3a1d7142f9d077
SugitaMasataka <mstk.ambit@gmail.com>
2021-10-03 17:16:14 +0900

    [add]コントローラー作成

---
+++ app/Http/Controllers/SlotInformationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\SlotInformation;
+
+class SlotInformationController extends Controller
+{
+    public function index (Request $request)
+    {
+        $slotInfos = SlotInformation::all();
+        return view ('index', ['slotInfos' => $slotInfos]);
+    }
+}
