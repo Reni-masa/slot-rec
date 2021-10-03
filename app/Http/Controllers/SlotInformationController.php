d1553b051902eae36239b914f97590d41b2eb5f7
SugitaMasataka <mstk.ambit@gmail.com>
2021-10-03 17:24:21 +0900

    [add]トップページ裏処理

--- app/Http/Controllers/SlotInformationController.php
+++ app/Http/Controllers/SlotInformationController.php
@@ -12,4 +12,5 @@ class SlotInformationController extends Controller
         $slotInfos = SlotInformation::all();
         return view ('index', ['slotInfos' => $slotInfos]);
     }
+
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>スロレク</title>
+    <title>SLOT-REC</title>
 </head>
 <body>
     <h1>スロレク</h1>
+
+    <ul>
+    @foreach ($slotInfos as $slotInfo)
+        <li><a href="">{{$slotInfo->slot_name}}</a></li>
+    @endforeach
+    </ul>
+
 </body>
 </html>
